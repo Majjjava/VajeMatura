e3c0e14ccc666728f4aa1cd3a2997d79b6a541a6
Majj <70351491+Majjjava@users.noreply.github.com>
2024-04-15 08:20:56 +0200

    Add files via upload

---
+++ Avto.java
@@ -0,0 +1,31 @@
+public class Avto {
+    //lastnosti
+    //tip ime
+//     String regSt;
+//     Static int poraba; //enako za celoten razred
+
+//     public void izpis(){
+//     System.out.println("******Avto******");
+//     System.out.println("Registrska: "+regSt);
+//     System.out.println("Poraba: "+poraba);
+//     }
+//     //konstruktor
+//     //nima tipa vrnjene vrednosti, ime mu je enako kot razredu
+//     public Avto(){
+//         System.out.println("Konst");
+//         regSt = "Neznana";
+//         poraba = 0;
+
+//         //podpis metordfe je stevilo in tip parametrov
+//     }
+//     public Avto(String r, int p){
+//         System.out.println("Konst s parametri");
+//         regSt = r;
+//         poraba = p;
+//     }
+//     public Avto(int p){
+//         System.out.println("Samo poraba");
+//         regSt = "";
+//         poraba=p;
+//     }
+ }
---
+++ prva.java
@@ -0,0 +1,17 @@
+public class prva {
+     public static void main(String[] args) {
+    //     //Ce nimam nobenega konstruktorja v Avto, se izvede konstruktor razreda object
+    //     Avto Fico = new Avto();
+    //     Fico.regSt = "Go 123";
+    //     Fico.poraba = 8;
+    //     Fico.izpis();
+    //     Avto a = new Avto("GO 777",9);
+    //     a.izpis();
+    // 
+    TestMetod a = new TestMetod();
+    a.Izpis(); //izpis vrača, kar je tipa void
+    a.vracanje(); //ni sintaktična napaka
+    int z = a.vracanje();
+
+    }
+}
